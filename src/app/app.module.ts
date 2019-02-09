e00c5a08dffa9a2119d9288d6f384382f1471133
Sergey Gusev <sgusev.dev@yandex.com>
2019-02-09 12:38:59 +0300

    setup bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule, // opt: bundle could be reduced by including only required bootstrap modules, like NgbPaginationModule, NgbAlertModule, ...
   ],
   providers: [],
   bootstrap: [AppComponent]
