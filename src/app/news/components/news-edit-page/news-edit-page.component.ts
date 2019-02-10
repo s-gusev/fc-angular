811122ce12e82490a23cecc886efd6b9180d0ef8
Sergey Gusev <sgusev.dev@yandex.com>
2019-02-10 12:42:52 +0300

    change title for create/edit page

--- src/app/news/components/news-edit-page/news-edit-page.component.ts
+++ src/app/news/components/news-edit-page/news-edit-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Article } from '../../models/article.model';
 
 @Component({
   selector: 'fc-news-edit-page',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-edit-page.component.css']
 })
 export class NewsEditPageComponent implements OnInit {
+  article: Article;
+  pageTitle!: string;
 
-  constructor() { }
+  constructor(
+    private newsService: NewsService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get("id");
+    if (id) {
+      this.pageTitle = "Edit News";
+      this.newsService.getById(id).subscribe(article => {
+        if (article) {
+          this.article = article;
+        } else {
+          this.router.navigateByUrl('/pagenotfound');
+        }
+      });
+    } else {
+      this.pageTitle = "Add News";
+    }
   }
-
 }
