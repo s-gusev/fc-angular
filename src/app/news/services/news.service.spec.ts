a744552683e395b6c6220f0208e218363c0a912b
Sergei Gusev <Sergei_Gusev1@epam.com>
2019-02-22 17:23:56 +0300

    fix news service test

--- src/app/news/services/news.service.spec.ts
+++ src/app/news/services/news.service.spec.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from './../../shared/services/auth.service';
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
@@ -6,7 +8,11 @@ import { NewsService } from './news.service';
 describe('Service: News', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [NewsService]
+      providers: [
+        NewsService,
+        { provide: AuthService, useValue: jasmine.createSpyObj('ValueService', ['getAuthHeders']) },
+        { provide: HttpClient, useValue: jasmine.createSpyObj('HttpClient', ['get', 'post', 'delete']) }
+      ],
     });
   });
 
