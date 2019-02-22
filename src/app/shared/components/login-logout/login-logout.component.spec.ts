a80116b2bcad88a6cd87fdfe0cc2326cb8e9722a
Sergei Gusev <Sergei_Gusev1@epam.com>
2019-02-22 20:25:52 +0300

    fix login-logout.component

--- src/app/shared/components/login-logout/login-logout.component.spec.ts
+++ src/app/shared/components/login-logout/login-logout.component.spec.ts
@@ -4,16 +4,29 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { LoginLogoutComponent } from './login-logout.component';
+import { RouterLinkDirectiveStub } from 'src/app/testing/router-link-directive-stub';
+import { AuthService } from '../../services/auth.service';
+import { MockAuthService } from 'src/app/testing/news/mock-auth-service';
+import { ActivatedRoute } from '@angular/router';
+import { MockActivatedRoute } from 'src/app/testing/news/mock-activated-route';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginLogoutComponent', () => {
   let component: LoginLogoutComponent;
   let fixture: ComponentFixture<LoginLogoutComponent>;
+  const locationSpy = jasmine.createSpyObj('Location', ['back']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginLogoutComponent ]
+      imports: [RouterTestingModule],
+      declarations: [LoginLogoutComponent, RouterLinkDirectiveStub ],
+      providers: [
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: Location, useValue: locationSpy },
+        { provide: ActivatedRoute, useClass: MockActivatedRoute }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
