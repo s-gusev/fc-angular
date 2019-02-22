f99daea4c3379911106e1933f11b880064aa1c4f
Sergei Gusev <Sergei_Gusev1@epam.com>
2019-02-22 20:07:14 +0300

    fix app.component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+
+@Component({ selector: 'fc-header', template: '' })
+class HeaderStubComponent { }
+
+@Component({ selector: 'fc-footer', template: '' })
+class FooterStubComponent { }
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,7 +16,9 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderStubComponent,
+        FooterStubComponent,
       ],
     }).compileComponents();
   }));
@@ -20,16 +29,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fc-angular'`, () => {
+  it(`should have header component`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fc-angular');
+    const headerComponent = fixture.debugElement.queryAll(de => de.componentInstance instanceof HeaderStubComponent);
+    expect(headerComponent).toBeTruthy();
+    expect(headerComponent.length).toEqual(1);
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should have footer Component', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to fc-angular!');
+    const footerComponent = fixture.debugElement.queryAll(de => de.componentInstance instanceof FooterStubComponent);
+    expect(footerComponent).toBeTruthy();
+    expect(footerComponent.length).toEqual(1);
   });
 });
